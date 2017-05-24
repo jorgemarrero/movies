0aeb888ae613d2ec952fb3d2e435de8e790c6f52
Jorge M <jorgems93@gmail.com>
2017-05-24 11:57:24 +0100

    Muestra el total de películas y permite filtrar por año y género.

--- js/controllers/home.controller.js
+++ js/controllers/home.controller.js
@@ -8,19 +8,77 @@
     HomeController.$inject = ['$scope', 'MoviesFactory'];
     function HomeController($scope, MoviesFactory) {
         var home = this;
-        home.movies = {};
-        
+        home.movies = [];
+        home.filtering = {
+            yearLower: 1900,
+            yearHigher: 2017,
+            imdbLower: 0,
+            imdbHigher: 10,
+            rtcsLower: 0,
+            rtcsHigher: 100,
+            rtasLower: 0,
+            rtasHigher: 100,
+            genres: []
+        }
+        home.totalResults = 0;
+
+
+
+
+        ////////////////
+        home.getFilteredMovies = getFilteredMovies;
+        home.filterByGenres = filterByGenres;
+        home.isGenreSelected = isGenreSelected;
+               
 
         activate();
 
         ////////////////
 
         function activate() {
-            MoviesFactory.getPopular().then(function(data){
-                home.movies = data;
-            });
-
+            getFilteredMovies();
         }
 
+        ////////////////
+
+        function getPopularMovies() {
+            MoviesFactory.getPopular().then(function(data){
+                home.totalResults = data.total;
+                home.movies = data.movies;
+                console.log(home.movies);
+            });
+        }
+        
+        function getFilteredMovies() {
+            MoviesFactory.getFiltered(home.filtering).then(function(data){
+                home.totalResults = data.total;
+                home.movies = data.movies;
+                console.log(home.movies);
+            });
+        }
+
+        function filterByGenres(genreId) {
+            var newGenre = true;
+            home.filtering.genres.forEach(function(element, position) {
+                if (element == genreId) {
+                    newGenre = false;
+                    home.filtering.genres.splice(position, 1);
+                }
+            });
+
+            if (newGenre) home.filtering.genres.push(genreId);
+            getFilteredMovies();
+        }
+
+        function isGenreSelected(genreId) {
+            var isSelected = false;
+            home.filtering.genres.forEach(function(element, position) {
+                if (element == genreId) {
+                    isSelected = true;
+                }
+            });
+
+            return isSelected;
+        }
     }
 })();
--- js/factories/movies.factory.js
+++ js/factories/movies.factory.js
@@ -8,8 +8,12 @@
     MoviesFactory.$inject = ['$http'];
     function MoviesFactory($http) {
         
+        var API_KEY = "7e2860525bbac2fa58d9b1fcb55830f9";
+
+
         return {
-            getPopular: getPopular
+            getPopular: getPopular,
+            getFiltered: getFiltered
         }
 
         /////////////////////////////////////////
@@ -17,12 +21,39 @@
         function getPopular() {
             return $http({
                 method : "GET",
-                url : "https://api.themoviedb.org/3/discover/movie?api_key=7e2860525bbac2fa58d9b1fcb55830f9&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+                url : "https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
             })
             .then (function (data) {
                 console.log(data);
-                return data.data.results;
+                var toReturn = [];
+                toReturn.total = data.data.total_results;
+                toReturn.movies = data.data.results;
+                return toReturn;
             });
+        }
+
+        function getFiltered(filter) {
+            console.log(filter);
+            
+            return $http({
+                method : "GET",
+                url : "https://api.themoviedb.org/3/discover/movie?api_key=" 
+                        + API_KEY
+                        + "&language=en-US"
+                        + "&sort_by=popularity.desc"
+                        + "&include_adult=false"
+                        + "&include_video=false"
+                        + "&page=1"
+                        + "&primary_release_date.gte=" + filter.yearLower
+                        + "&primary_release_date.lte=" + filter.yearHigher
+                        + "&with_genres=" + filter.genres.join()
+            })
+            .then (function (data) {
+                var toReturn = [];
+                toReturn.total = data.data.total_results;
+                toReturn.movies = data.data.results;
+                return toReturn;
+            });        
         }
         
     }
