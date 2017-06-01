b9bc4e4494055f646cd3b12e4a95ea8d964eac39
Jorge M <jorgems93@gmail.com>
2017-06-01 09:32:25 +0100

    Utilizando ngRoute

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-angular.module('pelisEOI', []);
+(function() {
+    'use strict';
+    angular.module('pelisEOI', ['ngRoute']).config(config);
+
+    config.$inject=['$routeProvider','$locationProvider'];
+
+    function config ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+
+        $routeProvider
+            .when("/", {
+                controller: 'HomeController as home',
+                templateUrl: '/views/home.html'
+            })
+            .when("/populares", {
+                controller: 'PopularController',
+                templateUrl: '/views/home.html'
+            })
+            .when("/proximamente", {
+                controller: 'ProximamenteController',
+                templateUrl: '/views/home.html'
+            })
+
+    }
+
+})();
