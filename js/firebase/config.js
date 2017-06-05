fa6bcc13765f244f35ef21fb487c8a0e90aef25a
Jorge M <jorgems93@gmail.com>
2017-06-05 09:49:55 +0100

    Configuración básica de  firebase

---
+++ js/firebase/config.js
@@ -0,0 +1,9 @@
+  var config = {
+    apiKey: "AIzaSyCNbXaJxMiyCeYe7hMEizEDbV9H6NvAi40",
+    authDomain: "movie-directory-917db.firebaseapp.com",
+    databaseURL: "https://movie-directory-917db.firebaseio.com",
+    projectId: "movie-directory-917db",
+    storageBucket: "movie-directory-917db.appspot.com",
+    messagingSenderId: "556529699163"
+  };
+  firebase.initializeApp(config);
