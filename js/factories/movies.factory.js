52ac082b5d81c7b15df976110cd390c5a1daf851
Jorge-SPC\Jorge <jorge.marrero106@alu.ulpgc.es>
2017-06-09 19:23:15 +0100

    Resultados sin región de ES

--- js/factories/movies.factory.js
+++ js/factories/movies.factory.js
@@ -80,7 +80,6 @@
                 url : "https://api.themoviedb.org/3/discover/movie?api_key=" 
                         + API_KEY
                         + "&language=es-ES"
-                        + "&region=ES" 
                         + "&sort_by=popularity.desc"
                         + "&include_adult=false"
                         + "&include_video=false"
